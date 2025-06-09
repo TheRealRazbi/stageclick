b903a67b3f2e22708823618ac789e9ade3efc112
Razbi <38946621+TheRealRazbi@users.noreply.github.com>
2025-06-09 18:05:11 +0300

    feat(porting): port steprunner. add smoketest

--- stageclick/__init__.py
+++ stageclick/__init__.py
@@ -10,6 +10,7 @@ from .image_processing import match_template, ScreenshotArea, screenshot_area, m
     create_load_template, find_color_in_image, split_screenshot_into_rows, get_main_monitor_bounding_box
 from .input_controllers import mouse, keyboard, MouseButton, MouseController, KeyboardController, KeyboardListener, \
     Key, alt_n, alt_y, alt_tab, ctrl_a, ctrl_up, ctrl_c, ctrl_s, ctrl_down, ctrl_right, run_listener, PauseHandler
+from .step_runner import *
 
 try:
     __version__ = version("stage-click")
@@ -21,4 +22,4 @@ __all__ = ["Window", "WindowClosed", "Button", "match_template", "ScreenshotArea
            "get_main_monitor_bounding_box", "__version__", "MouseController", "KeyboardController",
            "MouseButton", "KeyboardListener", "Key", "alt_n", "alt_y", "alt_tab", "ctrl_a", "ctrl_up",
            "ctrl_c", "ctrl_s", "ctrl_down", "ctrl_right", "run_listener", "PauseHandler", "WindowNotFound",
-           "TemplateNotFound"]
+           "TemplateNotFound", *step_runner.__all__]
